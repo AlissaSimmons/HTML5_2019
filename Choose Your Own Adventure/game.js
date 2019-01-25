7cec5a3f469064541c29bebddb61037f8c6bb29a
Alissa Simmons <alissa.s180@stu.nebo.edu>
2019-01-25 10:28:58 -0700

    We have programmed a base for input. It still needs to be customized to my story.

--- Choose Your Own Adventure/game.js
+++ Choose Your Own Adventure/game.js
@@ -14,6 +14,34 @@ Multiline comment
 
 // ("What type of pokemon do you like/");
 
-var pc = prompt("What is your name?");
+Game();
 
-confirm("So your name is " +pc);
+function Game(){
+    
+    document.write("Paint.");
+    var playerName = prompt("What is your name?");
+    alert("Welcome to the land of Drizdal "+ playerName);
+    alert("You wake up... DEAD! Or maybe not...");
+    
+Prison();
+
+    function Prison(){
+    var prison = prompt("yeet yeet boi u in prison I guess. /n -look around /n -go back to sleep / n taunt the gaurd").toLowerCase();
+    
+    if(prison == "look around" || prison == "look"){
+        var prisonLook = prompt("The prison is small, dirty and damp. A rat scurries into a hole in the corner. Bugs are everywhere and they are speaking to you.");
+    }
+    else if(prison == "go back to sleep" || prison == "sleep"){
+        alert("You slumber.");
+        var resume = confirm("Do you wish to continue?");
+        
+        if(resume){
+            Prison();
+        }
+        else{
+            alert("Game Over!");
+        }
+    }
+}
+
+}
