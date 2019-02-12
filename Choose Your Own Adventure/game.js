c904ed851b734ce3e3384e6f741666e731b78ef4
Alissa Simmons <alissa.s180@stu.nebo.edu>
2019-02-12 10:24:06 -0700

    Tried to fix the array situation. Game stops after two popups for some reason.

--- Choose Your Own Adventure/game.js
+++ Choose Your Own Adventure/game.js
@@ -1,56 +1,21 @@
-// Single line comment
-/*
-
-Multiline comment
-
-*/
-
-//document.write("<h1>I can't wait for <em>spring!</em></h1>");
-
-
-// alert("Warning! Will Robinson... Warning!");
-
-// confirm("Do you like pokemon?");
-
-// ("What type of pokemon do you like/");
-
-/*
-function Castle(){
-    var insideCstle = prompt(" upstairs - downstairs - courtyard").toLowerCase();
-    
-    switch(insideCastle){
-    case "upstairs" || "go upstairs":
-        var upstairs = prompt("You walk a long flight of stairs to the top floor");
-    }
-    break;
-    case "balcony":
-        alert("you go to the balcony");
-        Castle();
-    default:
-         alert("I'm not sure what "+insideCastle+" is");
-         Castle();
-    break;
-*/
-
 var inventory = {
     Coins: 10,
     Dagger: 0,
     Hearts: 0,
-    MushroomPeices: 0,
+    MushroomPeices: 0
 }
 
 Game();
 
 function Game(){
     
-    
     document.write("Paint.");
     var playerName = prompt("What is your title?");
     confirm("Velkommen, "+ playerName);
     
     var nickNames = ["Beezer", "Shrek", "Vanya", "Orco", "Peaches", "Munch"];
-    confirm("How about I call you "=gameNames[Math.floor(Math.random()*Math.floor*6)]+"?");
-    confirm("You really agree to being called " +nickNames+ " ? Wow, you really won't make it.")
+    confirm("How about I call you "+gameNames[Math.floor(Math.random()* 6)]+" instead?");
+    confirm("You really agree to being called " +nickNames+ " ? Wow, now I know you won't make it.");
     
     confirm("You wake up with hazy vision. After being still for a moment, the colors slowly seperate and you can make out basic shapes again.");
     
@@ -67,7 +32,7 @@ Bottomfloor();
         
         if(direction == "paintings" || "painting"){
             alert("There are five paintings.");
-            alert("The sign on the wall says do not touch, but there is an urge in the back of your mind that begs you to. At this rate, you're not sure you have a choice anymore.")
+            alert("The sign on the wall says do not touch, but there is an urge in the back of your mind that begs you to. At this rate, you're not sure you have a choice anymore.");
         }
         
     else if(Bottomfloor == "go back to sleep" || Bottomfloor == "sleep"){
