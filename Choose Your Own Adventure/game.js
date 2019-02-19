a0c3d82e50dc5bc2499fab2301b0a378e9562e2a
Alissa Simmons <alissa.s180@stu.nebo.edu>
2019-02-19 10:27:42 -0700

    Added a function or two. Trying to figure out where I've forgotten curly brackets and semicolons.

--- Choose Your Own Adventure/game.js
+++ Choose Your Own Adventure/game.js
@@ -1,10 +1,9 @@
 var inventory = {
-    Coins: 10,
     Dagger: 0,
     Hearts: 0,
     MushroomPeices: 0,
-    Torches: 0
-    
+    Feathers: 0,
+    Stars: 0
 }
 
 Game();
@@ -35,11 +34,15 @@ Bottomfloor();
     else if(Bottomfloor == "walk"){
         var direction = prompt("What do you wish to walk towards?");
         
+        if(direction == "upstaris" || "stairs"){
+            Upstairs();
+        }
+        
         if(direction == "paintings" || "painting"){
             alert("There are five paintings.");
             alert("The sign on the wall says do not touch, but there is an urge in the back of your mind that begs you to. At this rate, you're not sure you have a choice anymore.");
             
-            var choice = prompt("Although you don't have a choice but to touch one of the paintings, you do have a choice of which one. What painting do you coose?").toLowerCase();
+            var choice = prompt("Although you don't have a choice but to touch one of the paintings, you do have a choice of which one. There is a painting of a forest, the ocean, space, what appears to be someone of royalty, and a bear. So, what painting do you coose?").toLowerCase();
             
             if(choice == "forest" || "the forest painting" || "the forest one"){
                 Forest();
@@ -76,5 +79,25 @@ Bottomfloor();
                         alert("Farewell.");
                         Bottomfloor();
                         }
+    }
+    function Upstairs(){
+        alert("You are upstairs.");
+        alert("There is a painting here too, but it appears to be all blacked out.");
+        
+        var upstairsnow = prompt("Do you wish to leave or stay?");
+            if(upstairsnow == "leave");{
+                Bottomfloor();
+            }
+            
+            else if(upstairsnow == "stay");{
+                alert("You stare at the blank, black square for a while, pondering its purpose.");
+            }
+                
+        var touch? = prompt("Do you wish to touch this painting?");
+                if(touch == "yes"){
+                BlackPainting();
+            }
+                    
+                
     }
 }    
