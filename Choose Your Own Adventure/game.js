1ca53195276d642bc5f8fa5f016e81ed5b92e630
Alissa Simmons <alissa.s180@stu.nebo.edu>
2019-02-25 10:09:06 -0700

    It runs now, but I get a seemingly random "Game Over" Im figuring it out

--- Choose Your Own Adventure/game.js
+++ Choose Your Own Adventure/game.js
@@ -23,27 +23,29 @@ function Game() {
     
     alert("You wake up with hazy vision. After being still for a moment, the colors slowly seperate and you can make out basic shapes again.");
     
-function Bottomfloor() {
+    Bottomfloor();
+    
+    function Bottomfloor() {
         var bottomfloor = prompt("You sit up and find yourself in a white room. There is a door behind you, some paintings in front of you, and a set of stairs to the right. What would you like to do?").toLowerCase();
     
-    if(Bottomfloor == "look around" || Bottomfloor == "look") {
-        var bottomfloorLook = prompt("The room is quite small. There is a door behind you, some paintings in front of you, and a set of stairs to the right. Everything but the paintings is white.");
+        if (bottomfloor == "look around" || Bottomfloor == "look") {
+            var bottomfloorLook = prompt("The room is quite small. There is a door behind you, some paintings in front of you, and a set of stairs to the right. Everything but the paintings are white.");
         }
-         else if (Bottomfloor == "walk") {
+            if (bottomfloor == "walk") {
             var direction = prompt("What do you wish to walk towards?");
         }
         
-         else if(Bottomfloor == "go back to sleep" || Bottomfloor == "sleep") {
+            else if (bottomfloor == "go back to sleep" || Bottomfloor == "sleep") {
             alert("You slumber...");
             var resume = confirm("Do you wish to continue?");
-        }
+            }
         
-        if (resume) {
-            Bottomfloor();
-        }
-        else {
-            alert("Game Over!");
-        }
+                if (resume) {
+                Bottomfloor();
+            }
+                else {
+                alert("Game Over!");
+            }
     
        
         
@@ -51,7 +53,7 @@ function Bottomfloor() {
             Upstairs();
             }
         
-        if (direction == "paintings" || "painting"){
+        if (direction == "paintings" || "painting") {
             alert("There are five paintings.");
             alert("The sign on the wall says do not touch, but there is an urge in the back of your mind that begs you to. At this rate, you're not sure you have a choice anymore.");
             }
@@ -67,14 +69,14 @@ function Bottomfloor() {
         alert("You entered the forest painting. You look around and everything is made up of layered strokes of paint. The colors are vibrant.");
         
         var mushroom = prompt("Do you wish to take a peice of the mushroom?");
-        if(mushroom == "yes"){
-                    var mushroomtake = confirm("are you certain?");
+        if (mushroom == "yes") {
+        var mushroomtake = confirm("are you certain?");
                     }
-                        if(mushroomtake){
+                        if (mushroomtake) {
                         // ads =1 mushroom peice
-                        inventory.MushroomPeices ++;
+                        inventory.MushroomPeices++;
                         // Displays mushroom peices
-                        alert("You now have " +inventory.MushroomPeices+" Mushroom Peice(s).");
+                        alert("You now have " + inventory.MushroomPeices + " Mushroom Peice(s).");
                         Forest();
                         }
                         else{
